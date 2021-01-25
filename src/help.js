1d034fd3befb53e2d8d67c5f048023a3460f98cd
wendelkkk <76896941+wendelkkk@users.noreply.github.com>
2021-01-25 20:24:30 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,95 @@
+const help = (prefix) => {
+	return `
+「 *${botName}* 」
+
+◪ *informações*
+  ❏ Prefix: 「  ${prefix}  」
+  ❏ Criador : ${ownerBot}
+  ❏ sono da peste oq eu escrevo aqui?
+
+◪ *SOBRE*
+  │
+  ├─ ❏ ${prefix}info
+  ├─ ❏ ${prefix}blocklist
+  ├─ ❏ ${prefix}chatlist
+  ├─ ❏ ${prefix}ping
+  └─ ❏ ${prefix}bugreport
+
+◪ *FAZER*
+  │
+  ├─ ❏ ${prefix}sticker
+  ├─ ❏ ${prefix}stickergif
+  ├─ ❏ ${prefix}toimg
+  ├─ ❏ ${prefix}tomp3
+
+◪ *DOWNLOADER*
+  │
+  │─ ❏ *COMANDOS EM TESTE*
+  │
+  ├─ ❏ ${prefix}images
+  ├─ ❏ ${prefix}ytmp3
+  ├─ ❏ ${prefix}ytmp4
+  ├─ ❏ ${prefix}tiktok
+  └─ ❏ ${prefix}joox
+
+◪ *SOM*
+  │
+  ├─ ❏ ${prefix}play
+  └─ ❏ ${prefix}tts
+
+◪ *MÚSICA*
+  │
+  │─ ❏ *COMANDOS EM TESTE*
+  │
+  ├─ ❏ ${prefix}lirik
+  └─ ❏ ${prefix}chord
+
+◪ *WIBU*
+  │
+  │─ ❏ *COMANDOS EM TESTE*
+  │
+  ├─ ❏ ${prefix}neonime
+  ├─ ❏ ${prefix}pokemon
+  ├─ ❏ ${prefix}loli
+  ├─ ❏ ${prefix}waifu
+  ├─ ❏ ${prefix}randomanime
+  ├─ ❏ ${prefix}wait
+  └─ ❏ ${prefix}nekonime
+
+◪ *GROUP*
+  │
+  ├─ ❏ ${prefix}opengc
+  ├─ ❏ ${prefix}closegc
+  ├─ ❏ ${prefix}promote
+  ├─ ❏ ${prefix}demote
+  ├─ ❏ ${prefix}tagall
+  ├─ ❏ ${prefix}tagall2
+  ├─ ❏ ${prefix}tagall3
+  ├─ ❏ ${prefix}tagall4
+  ├─ ❏ ${prefix}tagall5
+  ├─ ❏ ${prefix}add
+  ├─ ❏ ${prefix}kick
+  ├─ ❏ ${prefix}listadmins
+  ├─ ❏ ${prefix}linkgroup
+  ├─ ❏ ${prefix}leave
+  ├─ ❏ ${prefix}welcome
+  ├─ ❏ ${prefix}nsfw
+  ├─ ❏ ${prefix}leveling
+  ├─ ❏ ${prefix}level
+  ├─ ❏ ${prefix}delete
+  ├─ ❏ ${prefix}simih
+  └─ ❏ ${prefix}ownergroup
+
+◪ *DONO*
+  │
+  ├─ ❏ ${prefix}setprefix
+  ├─ ❏ ${prefix}block
+  ├─ ❏ ${prefix}bc
+  ├─ ❏ ${prefix}bcgc
+  ├─ ❏ ${prefix}clone
+  └─ ❏ ${prefix}clearall
+
+`
+}
+
+exports.help = help
